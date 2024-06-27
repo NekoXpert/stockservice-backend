6426dcde6930b9c27510859c17ee3f2451b40c98
Felipe Reyes <felinekodev@gmail.com>
2024-06-27 01:29:49 -0500

    test

---
+++ src/main/java/com/webapp/stockservice_backend/StockserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.stockservice_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockserviceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/stockservice_backend/StockserviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.stockservice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockserviceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
