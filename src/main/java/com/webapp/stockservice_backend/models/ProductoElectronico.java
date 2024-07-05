a59194302ed8a60ac8fa605d67c5179861f62013
harolddelama <143916617+harolddelama@users.noreply.github.com>
2024-07-05 02:06:47 -0500

    Update ProductoElectronico.java

--- src/main/java/com/webapp/stockservice_backend/models/ProductoElectronico.java
+++ src/main/java/com/webapp/stockservice_backend/models/ProductoElectronico.java
@@ -5,5 +5,28 @@ import jakarta.persistence.*;
 @Entity
 @DiscriminatorValue("Electronics")
 public class ProductoElectronico extends Producto {
+    private String marca;
+    private String modelo;
 
+    // Getters y Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "Producto Electrónico: " + this.getNombre() + " - " + this.marca + " " + this.modelo;
+    }
 }
