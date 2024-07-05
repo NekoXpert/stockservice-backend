32ee3bf179eb52db389d3b9c8ec2821b5bdc9954
harolddelama <143916617+harolddelama@users.noreply.github.com>
2024-07-05 02:06:03 -0500

    Update ProductoComponentePC.java

--- src/main/java/com/webapp/stockservice_backend/models/ProductoComponentePC.java
+++ src/main/java/com/webapp/stockservice_backend/models/ProductoComponentePC.java
@@ -6,4 +6,28 @@ import jakarta.persistence.*;
 @DiscriminatorValue("Component")
 public class ProductoComponentePC extends Producto {
 
+    private String tipoComponente;
+    private String especificaciones;
+
+    // Getters y Setters
+    public String getTipoComponente() {
+        return tipoComponente;
+    }
+
+    public void setTipoComponente(String tipoComponente) {
+        this.tipoComponente = tipoComponente;
+    }
+
+    public String getEspecificaciones() {
+        return especificaciones;
+    }
+
+    public void setEspecificaciones(String especificaciones) {
+        this.especificaciones = especificaciones;
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "Componente PC: " + this.getNombre() + " - " + this.tipoComponente + " " + this.especificaciones;
+    }
 }
